db4f015c50738863e68fc7eb1333143795577aea
Nyasha Perry <nyashaperry2005@yahoo.com>
2020-12-29 01:46:24 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,6 +1,3 @@
 def first_challenge
-   epic_tragedy = {
-     :montague => {"Patriarch", "Matriach", "Hero", "Hero's Friends"}, 
-     :capulet => {"Patriach", "Matriarch", "Heroine", "Heroine's Friends"}
-   }
+   epic_tragedy = {}
 end
