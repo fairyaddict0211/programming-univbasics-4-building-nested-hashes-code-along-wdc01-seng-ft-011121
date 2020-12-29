65857fdb4957be707e6949bb116b5db91201375f
Nyasha Perry <nyashaperry2005@yahoo.com>
2020-12-29 02:04:22 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
-        :age => {}
+        :age => "50"
         },
       :matriarch => {
         :name => {}, 
